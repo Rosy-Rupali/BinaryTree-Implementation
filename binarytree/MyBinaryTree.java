65e2676073d86aa15e2d389dc6191d1308936a48
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-14 22:30:02 +0530

    added: displaying start message

---
+++ binarytree/MyBinaryTree.java
@@ -0,0 +1,7 @@
+package binarytree;
+
+public class MyBinaryTree<K extends Comparable<K>> {
+		public static void main(String args[]) {
+			System.out.println("Welcome to Binary tree implementation through java Generics");
+		}
+}
